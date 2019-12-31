5bba6667805429e8ebf96d388e5b2e3d8a59567e
Caleb Justice <calebjustice@MacBook-Pro.brywhi.com>
2019-12-30 19:56:05 -0700

    finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import Navbar from './components/Navbar';
+import CenterNavBar from './components/CenterNavBar';
+import HomePage from './components/HomePage';
+import SearchBar from './components/SearchBar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe("app component", () => {
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  })
+  it("navbar should always be on the website", () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.contains(<Navbar/>)).toBe(true);
+  })
+})
+
+
+describe("search bar", () => {
+  it("should not appear on home page", () => {
+    const wrapper = shallow(<HomePage />);
+    expect(wrapper.contains(<SearchBar/>)).toBe(false);
+  })
+  it("should appear on nav bar", () => {
+    const wrapper = shallow(<CenterNavBar />);
+    expect(wrapper.contains(<SearchBar/>)).toBe(true);
+  })
+})
