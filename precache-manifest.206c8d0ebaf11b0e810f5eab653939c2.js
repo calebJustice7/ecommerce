d316752bcd0e7f991fc217563d365559b31a21f8
Caleb Justice <calebjustice@Calebs-MacBook-Pro.local>
2020-02-05 09:37:51 -0700

    Updates

---
+++ precache-manifest.206c8d0ebaf11b0e810f5eab653939c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238e5422737b7115be3594df2b2ed3c7",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "f9e4193cedd953550d75",
+    "url": "/ecommerce/static/css/main.db205950.chunk.css"
+  },
+  {
+    "revision": "21536277a34a92d2eb33",
+    "url": "/ecommerce/static/js/2.f40472d5.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/ecommerce/static/js/2.f40472d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9e4193cedd953550d75",
+    "url": "/ecommerce/static/js/main.c815b152.chunk.js"
+  },
+  {
+    "revision": "fa5b56edb7f072adda96",
+    "url": "/ecommerce/static/js/runtime-main.f8ab206e.js"
+  },
+  {
+    "revision": "6991a1230b41a4171cd56729f550d262",
+    "url": "/ecommerce/static/media/home-page.6991a123.jpeg"
+  }
+]);
